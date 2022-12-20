173e2cdba847698e4c92b9f524cfab20eaeb0c89
Felipe <fehipe@gmail.com>
2022-12-20 16:55:40 +0100

    update pwa functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { SwupdaterService } from './sw-updater.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
+
 export class AppComponent {
-  title = 'angular-demo-app';
+
+  title = 'angular-demo-app DEVELOP';
+
+  constructor(private sw: SwupdaterService) { }
+
+  ngOnInit() {
+    this.sw.checkForUpdates();
+  }
 }
---
+++ src/app/environment.ts
@@ -0,0 +1,5 @@
+const environment: { production: boolean } = {
+    production: true
+};
+
+export { environment };
